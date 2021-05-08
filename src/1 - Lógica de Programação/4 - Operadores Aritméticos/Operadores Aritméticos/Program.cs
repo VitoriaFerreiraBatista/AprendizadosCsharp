95c0749365bbb14d9bc2e2a06b1762b217cd1047
Vitoria Batista <vitoria.batista@localiza.com>
2021-05-08 13:00:58 -0300

    Criando o projeto Operações Aritiméticas

---
+++ src/1 - Lógica de Programação/4 - Operadores Aritméticos/Operadores Aritméticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Operadores_Aritméticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Escreva a fórmula de Bhaskara e execute em console mostrando os resultados de ambas as raízes. O projeto tem como finalidade o estudo de operadores aritiméticos.");
+
+
+            int a = 1, b = -3, c = -4;
+            double delta = Math.Pow(b, 2) - (4 * a * c);
+            double bhaskara = Math.Sqrt(delta);
+            double X1 = (-b + bhaskara) / (2 * a);
+            double X2 = (-b - bhaskara) / (2 * a);
+            Console.WriteLine(X1 + " e " + X2) ;
+        }
+    }
+}
