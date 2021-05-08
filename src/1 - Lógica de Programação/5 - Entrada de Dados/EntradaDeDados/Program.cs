7b33c3301f25bbd9912bb5556a055d20f33715f4
Vitoria Batista <vitoria.batista@localiza.com>
2021-05-08 14:16:13 -0300

    Criando o Projeto Entrada de Dados

---
+++ src/1 - Lógica de Programação/5 - Entrada de Dados/EntradaDeDados/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faça um programa em console, que receba do usuário o texto: banana maçã uva morango e imprima cada palavra dessas palavras em uma linha separada");
+            Console.WriteLine("Escreva o texto a ser separado");
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string vet1 = vet[0];
+            string vet2 = vet[1];
+            string vet3 = vet[2];
+            string vet4 = vet[3];
+
+            Console.WriteLine(vet1);
+            Console.WriteLine(vet2);
+            Console.WriteLine(vet3);
+            Console.WriteLine(vet4);
+
+        }
+    }
+}
