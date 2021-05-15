b4e8bdab32c3efdb94fcd11b88227f67874a1138
Vitoria Batista <vitoria.batista@localiza.com>
2021-05-15 17:16:01 -0300

    Criando o projeto Estrutura de Repetição

---
+++ src/1 - Lógica de Programação/9 - Estrutura de Repetição/EstruturadeRepeticao/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EstruturadeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero = 1;
+
+            while (numero > 0)
+            {
+                Console.WriteLine("Digite um número");
+                numero = int.Parse(Console.ReadLine());
+
+
+                if (numero < 0)
+                {
+                    Console.WriteLine("Numero negativo!");
+                }
+                else
+                {
+                    double raizQuadrada = Math.Sqrt(numero);
+                    Console.WriteLine(raizQuadrada.ToString("F3"));
+                }
+            }
+        }
+    }
+}
