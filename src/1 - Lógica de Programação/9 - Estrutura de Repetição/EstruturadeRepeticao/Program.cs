fd7f7d2742526425882dc745e9c0d1b45d0543dd
Vitoria Batista <vitoria.batista@localiza.com>
2021-05-15 19:56:07 -0300

    Implementando a estrutura de repetição FOR

--- src/1 - Lógica de Programação/9 - Estrutura de Repetição/EstruturadeRepeticao/Program.cs
+++ src/1 - Lógica de Programação/9 - Estrutura de Repetição/EstruturadeRepeticao/Program.cs
@@ -6,25 +6,40 @@ namespace EstruturadeRepeticao
     {
         static void Main(string[] args)
         {
+            int numero = 0;
 
-            int numero = 1;
-
-            while (numero > 0)
+            // Estrutura de Repetição [ While ]
+            while (numero >= 0)
             {
-                Console.WriteLine("Digite um número");
+                Console.WriteLine("[while] Digite um número: ");
                 numero = int.Parse(Console.ReadLine());
-
-
-                if (numero < 0)
-                {
-                    Console.WriteLine("Numero negativo!");
-                }
-                else
-                {
-                    double raizQuadrada = Math.Sqrt(numero);
-                    Console.WriteLine(raizQuadrada.ToString("F3"));
-                }
+                Console.WriteLine("Repete [while]");
             }
+
+            // Estrutura de Repetição [ Do - While ]
+            do
+            {
+                Console.WriteLine("[do-while] Digite um número: ");
+                numero = int.Parse(Console.ReadLine());
+                Console.WriteLine("Repete [do-while]");
+            } while (numero >= 0);
+
+            // Estrutura de Repetição [ For ]
+
+            Console.WriteLine("Digite a quantidade de numeros que serão somados:");
+            int quantidade = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                Console.WriteLine(" Digite o "+ (i+1) +"º numero:");
+                soma += int.Parse(Console.ReadLine());
+               
+            }
+            Console.WriteLine(" O valor da soma é: " + soma);
         }
+
+
     }
+
 }
