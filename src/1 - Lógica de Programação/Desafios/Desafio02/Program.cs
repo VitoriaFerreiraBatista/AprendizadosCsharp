3829f74506ca83980243c0dca39cbca6fffcaf09
Vitoria Batista <vitoria.batista@localiza.com>
2021-05-22 12:08:04 -0300

    Desafio 02

---
+++ src/1 - Lógica de Programação/Desafios/Desafio02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o diâmetro do círculo");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) / 2;
+            double area =  Math.Pow(raio, 2) * 3.14159;
+            Console.WriteLine("A = " + area.ToString("F4", CultureInfo.InvariantCulture));
+                    
+        }
+    }
+}
