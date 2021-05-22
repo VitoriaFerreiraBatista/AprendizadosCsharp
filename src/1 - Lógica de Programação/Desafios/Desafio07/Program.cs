f67140c96b5c95c1379ef959a7b5f9548f4bae0c
Vitoria Batista <vitoria.batista@localiza.com>
2021-05-22 16:38:43 -0300

    Desafio 08

---
+++ src/1 - Lógica de Programação/Desafios/Desafio07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Desafio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número inteiro para verificarmos se o mesmo é negativo:");
+
+            int y = int.Parse(Console.ReadLine());
+            if (y > 0)
+            {
+                Console.WriteLine("NÃO NEGATIVO");
+
+
+            }
+            else
+            {
+                Console.WriteLine("NEGATIVO");
+
+            }
+        }
+    }
+}
---
+++ src/1 - Lógica de Programação/Desafios/Desafio08/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Desafio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número inteiro para verificarmos se o mesmo é par ou ímpar:");
+
+            int y = int.Parse(Console.ReadLine());
+            if (y % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+
+
+            }
+            else
+            {
+                Console.WriteLine("ÍMPAR");
+            }
+        }
+    }
+}
