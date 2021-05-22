cf9972fc564191de4adc814d1497a9fc822b169c
Vitoria Batista <vitoria.batista@localiza.com>
2021-05-22 11:08:22 -0300

    Desafio 01

---
+++ src/1 - Lógica de Programação/Desafios/Desafio01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Desafio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escreva dois valores que serão somados");
+            int valor1 = int.Parse(Console.ReadLine());
+            int valor2 = int.Parse(Console.ReadLine());
+            int resultado = valor1 + valor2;
+            Console.WriteLine("SOMA = " + resultado);
+        }
+    }
+}
