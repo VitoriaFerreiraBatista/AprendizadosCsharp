2ff1b3c8d86dc166cb3d3c5567fcb59be1cd900b
Vitoria Batista <vitoria.batista@localiza.com>
2021-05-22 12:24:51 -0300

    Desafio 03

---
+++ src/1 - Lógica de Programação/Desafios/Desafio03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Desafio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Digite 4 valores inteiros");
+            int A = int.Parse(Console.ReadLine()), B = int.Parse(Console.ReadLine()), C = int.Parse(Console.ReadLine()), D = int.Parse(Console.ReadLine());
+            int resultado = (A * B) - (C * D);
+            Console.WriteLine("DIFERENÇA = " + resultado);
+        }
+    }
+}
