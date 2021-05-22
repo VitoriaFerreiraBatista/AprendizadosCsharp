97b6daaeb1e1889bab2a02d7b35011b0b5c3c9f9
Vitoria Batista <vitoria.batista@localiza.com>
2021-05-22 12:47:52 -0300

    Desafio 04

---
+++ src/1 - Lógica de Programação/Desafios/Desafio04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Digite o número do colaborador");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Digite a carga horária do colaborador");
+            int horas = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Digite o valor/hora do colaborador");
+            double valorh = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salario = horas * valorh;
+            Console.WriteLine("Número: "+ numero+" Salário: R$ "+ salario.ToString("F2"));
+        }
+    }
+}
