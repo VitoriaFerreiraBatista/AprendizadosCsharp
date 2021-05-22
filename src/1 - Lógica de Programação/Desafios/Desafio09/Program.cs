bb0794ff5455b2865540d35c5b60f6467b304178
Vitoria Batista <vitoria.batista@localiza.com>
2021-05-22 17:58:05 -0300

    Desafio 09 e Desafio 10

---
+++ src/1 - Lógica de Programação/Desafios/Desafio09/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Desafio09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um valor inteiro para A: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Informe um valor inteiro para B: ");
+            int B = int.Parse(Console.ReadLine());
+
+            if (A == 0 || B == 0 || B % A == 0 || A % B == 0)
+            {
+                Console.WriteLine("São múltiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são múltiplos");
+            }
+        }
+    }
+}
---
+++ src/1 - Lógica de Programação/Desafios/Desafio10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Desafio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a hora inicial do jogo: ");
+            double inicio = double.Parse(Console.ReadLine());
+            Console.Write("Informe a hora final do jogo: ");
+            double fim = double.Parse(Console.ReadLine());
+
+            if (inicio == fim)
+            {
+                Console.WriteLine("O jogo durou 24 horas");
+            }
+
+            else if (fim < inicio)
+            {
+                double resultado = (fim + 24) - inicio;
+                Console.WriteLine("O jogo durou " + resultado + " horas");
+            }
+            else
+            {
+                double resultado = fim - inicio;
+                Console.WriteLine("O jogo durou " + resultado + " horas");
+            }
+        }
+    }
+}
