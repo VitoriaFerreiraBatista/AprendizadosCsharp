9b8d115d8453c59ad565fad38a9e2878ba145925
Vitoria Batista <vitoria.batista@localiza.com>
2021-06-06 09:11:09 -0300

    Clases Atributos e Métodos

---
+++ src/2 - POO/1 - Classes, Atributos e Métodos/bin/ClassesAtributosEMétodos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassesAtributosEMétodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            
+            Console.Write("Preco: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade: ");
+            produto.Quantidade= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + produto);
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados atualizados: " + produto);
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados atualizados: " + produto);
+
+
+
+
+
+
+        }
+
+    }
+}
