a777aca3fb7e7f57315c215697f81a46ec948c10
Vitoria Batista <vitoria.batista@localiza.com>
2021-06-06 15:33:15 -0300

    Encapsulamento

---
+++ src/2 - POO/3 - Encapsulamento/Encapsulamento/Produto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    class Produto
+    {
+
+        // Ordem sugerida para criar classe:
+        // Atributos privados
+        // Proprioedades autoimplementadas
+        // Construtores
+        // Propriedades customizadas
+        // Outros métodos da classe
+
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+
+            set
+            {
+                if (value != null)
+                    _nome = value;
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            double P = Preco * Quantidade;
+            return P;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+
+        }
+
+        public override string ToString()
+        {
+            return _nome + ", R$: " + Preco.ToString("F2") + ", " + Quantidade + " unidades" + ", Total R$: " + ValorTotalEmEstoque().ToString("F2");
+        }
+    }
+}
---
+++ src/2 - POO/3 - Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preco: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto produto = new Produto(nome, preco, quantidade);
+
+            produto.Nome = "Notebook";
+
+            Console.WriteLine("Dados do produto: " + produto);
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados atualizados: " + produto);
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados atualizados: " + produto);
+        }
+    
+    }
+}
